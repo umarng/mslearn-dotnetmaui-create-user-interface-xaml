708a1b6aaaac051daba1e4395c0bf9436bf6d67a
ASUS <ASUS@PC-FORM-04>
2024-05-06 19:17:00 +0000

    first commit

---
+++ exercise1/Notes/SharedResources.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    internal class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+
+        public static readonly Color BackgroundColor = Color.FromRgb(0xFF, 0xF0, 0xAD);
+
+
+    }
+}
